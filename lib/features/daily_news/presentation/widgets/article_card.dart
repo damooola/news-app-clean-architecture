eb83b5b2c8d0a4d91ac0f236cb77a5ca4e51c70e
damooola <damolanelson@gmail.com>
2025-03-18 01:39:02 +0100

    add url launcher in ontap

--- lib/features/daily_news/presentation/widgets/article_card.dart
+++ lib/features/daily_news/presentation/widgets/article_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/utils/url_launcher_util.dart';
 import 'package:news_app/features/daily_news/domain/entities/article_entity.dart';
 
 class ArticleCard extends StatelessWidget {
@@ -8,47 +9,51 @@ class ArticleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (article.urlToImage != null) Image.network(article.urlToImage!),
-            const SizedBox(height: 8.0),
-            Text(
-              article.title ?? 'No Title',
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () => launchArticleUri(context, article),
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (article.urlToImage != null)
+                Image.network(article.urlToImage!),
+              const SizedBox(height: 8.0),
+              Text(
+                article.title ?? 'No Title',
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              article.description ?? 'No Description',
-              style: const TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey,
+              const SizedBox(height: 8.0),
+              Text(
+                article.description ?? 'No Description',
+                style: const TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey,
+                ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              'By ${article.author ?? 'Unknown'}',
-              style: const TextStyle(
-                fontSize: 12.0,
-                fontStyle: FontStyle.italic,
+              const SizedBox(height: 8.0),
+              Text(
+                'By ${article.author ?? 'Unknown'}',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  fontStyle: FontStyle.italic,
+                ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              article.publishedAt ?? 'No Date',
-              style: const TextStyle(
-                fontSize: 12.0,
-                color: Colors.grey,
+              const SizedBox(height: 8.0),
+              Text(
+                article.publishedAt ?? 'No Date',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.grey,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
