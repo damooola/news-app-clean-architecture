6bc8b9b458c2a05aa6bb5e4db75df31dddcafa64
damooola <damolanelson@gmail.com>
2025-03-18 01:38:24 +0100

    use http.get instead of retrofit and dio

--- lib/features/daily_news/data/data_sources/remote/news_api_service.dart
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -1,18 +1,26 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:news_app/core/constants/constants.dart';
 import 'package:news_app/features/daily_news/data/models/article.dart';
-import 'package:retrofit/retrofit.dart';
 
-part 'news_api_service.g.dart';
+class NewsApiService {
+  final String _baseUrl = newsApiBaseUrl;
 
-@RestApi(baseUrl: newsApiBaseUrl)
-abstract class NewsApiService {
-  factory NewsApiService(Dio dio) = _NewsApiService;
-  
-  @GET('/top-headlines')
-  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
-    @Query("apiKey") String? apiKey,
-    @Query("country") String? country,
-    @Query("category") String? category,
-  });
+  Future<List<ArticleModel>> getNewsArticles({
+    String? apiKey,
+    String? country,
+    String? category,
+  }) async {
+    final response = await http.get(
+      Uri.parse(
+          '$_baseUrl/top-headlines?country=$country&apiKey=$apiKey&category=$category'),
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body)['articles'];
+      return jsonData.map((json) => ArticleModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load articles');
+    }
+  }
 }
