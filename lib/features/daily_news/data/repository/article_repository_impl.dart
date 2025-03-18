c8588f7ec08ff03de2f786ff6326c9ccdb277b93
damooola <damolanelson@gmail.com>
2025-03-18 01:39:31 +0100

    new commit

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
 import 'package:news_app/core/constants/constants.dart';
 import 'package:news_app/core/resources/data_state.dart';
 import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
@@ -11,27 +8,18 @@ class ArticleRepositoryImpl implements ArticleRepository {
   final NewsApiService _newsApiService;
 
   ArticleRepositoryImpl(this._newsApiService);
+
   @override
   Future<DataState<List<ArticleModel>>> getNewsArticles() async {
     try {
-      final httpResponse = await _newsApiService.getNewsArticles(
+      final articles = await _newsApiService.getNewsArticles(
         apiKey: newsApiKey,
         country: countryQuery,
         category: categoryQuery,
       );
-      if (httpResponse.response.statusCode == HttpStatus.ok) {
-        return DataSuccess(httpResponse.data);
-      } else {
-        return DataFailed(
-          DioException(
-              error: httpResponse.response.statusMessage,
-              response: httpResponse.response,
-              type: DioExceptionType.badResponse,
-              requestOptions: httpResponse.response.requestOptions),
-        );
-      }
-    } on DioException catch (error) {
-      return DataFailed(error);
+      return DataSuccess(articles);
+    } catch (error) {
+      return DataFailed(Exception(error));
     }
   }
 }
